b6e8544352311b987be7d611c7a1c260486f88a4
Boris Barbulovski <bbarbulovski@gmail.com>
2023-02-05 12:10:57 +0100

    Update json definition.

--- definitions/json_definition.h
+++ definitions/json_definition.h
@@ -12,6 +12,8 @@ enum text_parser_json_tags {
     TextParser_json_Operator,
     TextParser_json_Boolean,
     TextParser_json_KeySeparator,
+    TextParser_json_ValueSeparator,
+    TextParser_json_Error = -1,
 };
 
 static const language_definition_t json_definition = {
@@ -23,16 +25,17 @@ static const language_definition_t json_definition = {
     .can_have_other_text_inside = false,
     .tokens = (textparse_token_t[]) {
         {.name = "jsonObject",    .start_string = "\\{",                                         .end_string = "\\}",     .only_start_tag = false, .multi_line = true,  .can_have_other_text_inside = false, .end_tag_is_optional = false,
-            .nested_tokens = (int []) {TextParser_json_String, TextParser_json_Number, TextParser_json_Operator, TextParser_json_Boolean, TextParser_json_KeySeparator, TextParser_END}
+            .nested_tokens = (int []) {TextParser_json_String, TextParser_json_Number, TextParser_json_Operator, TextParser_json_Boolean, TextParser_json_KeySeparator, TextParser_json_ValueSeparator, TextParser_END}
         },
         {.name = "jsonArray",     .start_string = "\\[",                                         .end_string = "\\]",     .only_start_tag = false, .multi_line = true,  .can_have_other_text_inside = false, .end_tag_is_optional = false,
-            .nested_tokens = (int []) {TextParser_json_Object, TextParser_json_String, TextParser_json_Number, TextParser_json_Operator, TextParser_json_Boolean, TextParser_END}
+            .nested_tokens = (int []) {TextParser_json_Object, TextParser_json_String, TextParser_json_Number, TextParser_json_Operator, TextParser_json_Boolean, TextParser_json_ValueSeparator, TextParser_END}
         },
         {.name = "String",        .start_string = "\\\"",                                        .end_string = "\\\"",    .only_start_tag = false, .multi_line = false, .can_have_other_text_inside = true,  .end_tag_is_optional = false },
         {.name = "Number",        .start_string = "[-+]?[0-9]+\\.?[0-9]*",                       .end_string = NULL,      .only_start_tag = true,  .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
         {.name = "Operator",      .start_string = "=",                                           .end_string = NULL,      .only_start_tag = true,  .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
         {.name = "Boolean",       .start_string = "true|false",                                  .end_string = NULL,      .only_start_tag = true,  .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
         {.name = "KeySeparator",  .start_string = ":",                                           .end_string = NULL,      .only_start_tag = true,  .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
+        {.name = "ValueSeparator",.start_string = ",",                                           .end_string = NULL,      .only_start_tag = true,  .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
         {.name = NULL,            .start_string = NULL,                                          .end_string = NULL,      .only_start_tag = false, .multi_line = false, .can_have_other_text_inside = false, .end_tag_is_optional = false },
     },
 };
