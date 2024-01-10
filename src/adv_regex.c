5265ab9a69f208f88ff237eaf09ff15b31bd63e3
Boris Barbulovski <bbarbulovski@gmail.com>
2024-01-10 03:28:49 +0100

    Use PCRE2_CODE_UNIT_WIDTH 0 so we can use all char widths.

--- src/adv_regex.c
+++ src/adv_regex.c
@@ -1,4 +1,4 @@
-#define PCRE2_CODE_UNIT_WIDTH 8
+#define PCRE2_CODE_UNIT_WIDTH 0
 
 #include "adv_regex.h"
 #include <pcre2.h>
@@ -16,23 +16,23 @@ int adv_regex_find_pattern(const char *regex, int encoding, const char *start, i
 
     PCRE2_SIZE erroroffset = 0;
     int errornumber = 0;
-    pcre2_match_data *match_data = NULL;
-    pcre2_code *re = NULL;
+    pcre2_match_data_8 *match_data = NULL;
+    pcre2_code_8 *re = NULL;
     PCRE2_SIZE *ovector = NULL;
     int rc = 0;
 
-    re = pcre2_compile((PCRE2_SPTR8)regex, PCRE2_ZERO_TERMINATED, 0, &errornumber, &erroroffset, NULL);                 /* use default compile context */
+    re = pcre2_compile_8((PCRE2_SPTR8)regex, PCRE2_ZERO_TERMINATED, 0, &errornumber, &erroroffset, NULL);                 /* use default compile context */
     if (re == NULL)
     {
-        PCRE2_UCHAR buffer[256];
-        pcre2_get_error_message(errornumber, buffer, sizeof(buffer));
+        PCRE2_UCHAR8 buffer[256];
+        pcre2_get_error_message_8(errornumber, buffer, sizeof(buffer));
         printf("PCRE2 compilation failed at offset %zu: %s\n", erroroffset, buffer);
         return -1;
     }
 
-    match_data = pcre2_match_data_create_from_pattern(re, NULL);
+    match_data = pcre2_match_data_create_from_pattern_8(re, NULL);
 
-    rc = pcre2_match(
+    rc = pcre2_match_8(
         re,                   /* the compiled pattern */
         (PCRE2_SPTR8)start,   /* the subject string */
         max_len,              /* the length of the subject */
@@ -42,7 +42,7 @@ int adv_regex_find_pattern(const char *regex, int encoding, const char *start, i
         NULL);                /* use default match context */
     if (rc == 1)
     {
-        ovector = pcre2_get_ovector_pointer(match_data);
+        ovector = pcre2_get_ovector_pointer_8(match_data);
         if (ovector)
         {
             ret = ovector[0];
@@ -53,9 +53,9 @@ int adv_regex_find_pattern(const char *regex, int encoding, const char *start, i
     }
 
     if (match_data)
-        pcre2_match_data_free(match_data);
+        pcre2_match_data_free_8(match_data);
 
-    pcre2_code_free(re);
+    pcre2_code_free_8(re);
 
     return ret;
 }
