d98254849639ea02c6c6286747cb23cde80a7402
Boris Barbulovski <bbarbulovski@gmail.com>
2024-01-19 01:24:58 +0100

    Update CFML defitinion.

--- definitions/cfml_definition.h
+++ definitions/cfml_definition.h
@@ -5,34 +5,39 @@
 
 
 enum text_parser_cfml_tags {
-    TextParser_cfml_ScriptTag,
-    TextParser_cfml_OutputTag,
+    TextParser_cfml_ScriptTagPair,
+    TextParser_cfml_ScriptStartTag,
+    TextParser_cfml_ScriptEndTag,
+    TextParser_cfml_OutputTagPair,
+    TextParser_cfml_OutputStartTag,
+    TextParser_cfml_OutputEndTag,
     TextParser_cfml_StartTag,
     TextParser_cfml_EndTag,
     TextParser_cfml_Comment,
     TextParser_cfml_SingleString,
+
     TextParser_cfml_DoubleString,
     TextParser_cfml_SingleChar,
     TextParser_cfml_DoubleChar,
     TextParser_cfml_SharpChar,
-
     TextParser_cfml_SharpExpression,
     TextParser_cfml_Expression,
     TextParser_cfml_ScriptExpression,
     TextParser_cfml_OutputExpression,
     TextParser_cfml_ScriptBlockComment,
     TextParser_cfml_ScriptLineComment,
+
     TextParser_cfml_ExpressionEnd,
     TextParser_cfml_Number,
     TextParser_cfml_Boolean,
     TextParser_cfml_ObjectMember,
-
     TextParser_cfml_Function,
     TextParser_cfml_Separator,
     TextParser_cfml_Variable,
     TextParser_cfml_Object,
     TextParser_cfml_Assigment,
     TextParser_cfml_Operator,
+
     TextParser_cfml_SubExpression,
     TextParser_cfml_VariableIndex,
     TextParser_cfml_CodeBlock,
@@ -45,30 +50,62 @@ static const language_definition cfml_definition = {
     .case_sensitivity = false,
     .default_file_extensions = (const char *[]) {"cfm", "cfc", NULL},
     .default_text_encoding = ADV_REGEX_TEXT_LATIN1,
-    .starts_with = (int []) {TextParser_cfml_ScriptTag,
-                             TextParser_cfml_OutputTag,
+    .starts_with = (int []) {TextParser_cfml_ScriptTagPair,
+                             TextParser_cfml_OutputTagPair,
                              TextParser_cfml_StartTag,
                              TextParser_cfml_EndTag,
                              TextParser_cfml_Comment,
                              TextParser_END},
     .tokens = (textparser_token[]) {
         {
-            .name = "ScriptTag",
-            .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
-            .start_string = "<cfscript>",
-            .end_string = "<\\/cfscript>",
+            .name = "ScriptTagPair",
+            .type = TEXTPARSER_TOKEN_TYPE_GROUP_ALL_CHILDREN_IN_SAME_ORDER,
             .multi_line = true,
-            .nested_tokens = (int []) {TextParser_cfml_ScriptExpression,
+            .nested_tokens = (int []) {TextParser_cfml_ScriptStartTag,
+                                       TextParser_cfml_ScriptExpression,
+                                       TextParser_cfml_ScriptEndTag,
                                        TextParser_END}
         },
         {
-            .name = "OutputTag",
+            .name = "ScriptStartTag",
             .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
-            .start_string = "<cfoutput>",
-            .end_string = "<\\/cfoutput>",
+            .start_string = "<cfscript",
+            .end_string = ">",
             .multi_line = true,
-            .nested_tokens = (int []) {TextParser_cfml_OutputExpression,
+            .nested_tokens = (int []) {TextParser_cfml_Expression,
                                        TextParser_END}
+        },
+        {
+            .name = "ScriptEndTag",
+            .type = TEXTPARSER_TOKEN_TYPE_SIMPLE_TOKEN,
+            .start_string = "<\\/cfscript>"
+        },
+        {
+            .name = "OutputTagPair",
+            .type = TEXTPARSER_TOKEN_TYPE_GROUP_ALL_CHILDREN_IN_SAME_ORDER,
+            .multi_line = true,
+            .nested_tokens = (int []) {TextParser_cfml_OutputStartTag,
+                                       TextParser_cfml_OutputExpression,
+                                       TextParser_cfml_OutputEndTag,
+                                       TextParser_END}
+        },
+        {
+            .name = "OutputStartTag",
+            .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
+            .start_string = "<cfoutput",
+            .end_string = ">",
+            .multi_line = true,
+            .nested_tokens = (int []) {TextParser_cfml_ScriptTagPair,
+                                       TextParser_cfml_OutputTagPair,
+                                       TextParser_cfml_StartTag,
+                                       TextParser_cfml_EndTag,
+                                       TextParser_cfml_Comment,
+                                       TextParser_END}
+        },
+        {
+            .name = "OutputEndTag",
+            .type = TEXTPARSER_TOKEN_TYPE_SIMPLE_TOKEN,
+            .start_string = "<\\/cfoutput>"
         },
         {
             .name = "StartTag",
@@ -107,6 +144,7 @@ static const language_definition cfml_definition = {
                                        TextParser_cfml_SingleChar,
                                        TextParser_END}
         },
+        ////////////////////////////////
         {
             .name = "DoubleString",
             .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
@@ -134,7 +172,6 @@ static const language_definition cfml_definition = {
             .type = TEXTPARSER_TOKEN_TYPE_SIMPLE_TOKEN,
             .start_string = "##",
         },
-        ////////////////////////////////
         {
             .name = "SharpExpression",
             .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
@@ -219,6 +256,7 @@ static const language_definition cfml_definition = {
             .start_string = "\\/\\/.*",
             .immediate_start = true,
         },
+        ////////////////////////////////
         {
             .name = "ExpressionEnd",
             .type = TEXTPARSER_TOKEN_TYPE_SIMPLE_TOKEN,
@@ -243,7 +281,6 @@ static const language_definition cfml_definition = {
             .start_string = "\\.",
             .immediate_start = true,
         },
-        ////////////////////////////////
         {
             .name = "Function",
             .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
@@ -275,7 +312,7 @@ static const language_definition cfml_definition = {
             .name = "Assigment",
             .type = TEXTPARSER_TOKEN_TYPE_SIMPLE_TOKEN,
             .start_string = "(=|\\+=|\\-=|\\*=|/=|%=|&=)",
-            .immediate_start = false,
+            .immediate_start = true,
         },
         {
             .name = "Operator",
@@ -283,6 +320,7 @@ static const language_definition cfml_definition = {
             .start_string = "(\\bgreater\\s+than\\s+or\\s+equal\\s+to\\b|\\bless\\s+than\\s+or\\s+equal\\s+to\\b|\\bdoes\\s+not\\s+contain\\b|\\bgreater\\s+than\\b|\\bnot\\s+equal\\b|\\bless\\s+than\\b|\\bcontains\\b|\\bis\\s+not\\b|\\bequal\\b|\\+\\+|\\bmod\\b|\\bnot\\b|\\band\\b|\\bxor\\b|\\beqv\\b|\\bgte\\b|\\blte\\b|\\bimp\\b|\\bneq\\b|\\bis\\b|\\bor\\b|\\bgt\\b|\\bge\\b|\\blt\\b|\\ble\\b|--|\\+|\\*|\\|\\^|&&|\\|\\|\\beq\\b|\\?|-|/|&|%|:|!|=)",
             .immediate_start = true,
         },
+        ////////////////////////////////
         {
             .name = "SubExpression",
             .type = TEXTPARSER_TOKEN_TYPE_START_STOP,
@@ -316,7 +354,6 @@ static const language_definition cfml_definition = {
             .immediate_start = true,
             .start_string = "(\\bvar\\b|\\bfunction\\b|\\bthis\\b|try\\b|\\bcatch\\b|\\bif\\b|\\bthen\\b|\\belse\\b)"
         },
-        ////////////////////////////////
         {.name = NULL},
     },
 };
