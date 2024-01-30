e3caea0a14d67f18367864f1e0b90836d2fcd72c
Boris Barbulovski <bbarbulovski@gmail.com>
2024-01-30 04:57:37 +0100

    Use #pragma once.

--- include/adv_regex.h
+++ include/adv_regex.h
@@ -1,5 +1,4 @@
-#ifndef ADV_REGEX_H
-#define ADV_REGEX_H
+#pragma once
 
 #include <stdbool.h>
 #include <stddef.h>
@@ -9,5 +8,3 @@ enum adv_regex_encoding { ADV_REGEX_TEXT_ERROR, ADV_REGEX_TEXT_LATIN1, ADV_REGEX
 
 bool adv_regex_find_pattern(const char *regex_str, void **regex, enum adv_regex_encoding encoding, const char *start, size_t max_len, size_t *offset, size_t *length);
 void adv_regex_free(void **regex, enum adv_regex_encoding encoding);
-
-#endif // ADV_REGEX_H
