d413e434e6b25aff7be5f1679c37a4ae56550273
Boris Barbulovski <bbarbulovski@gmail.com>
2025-12-15 03:47:34 +0100

    Add CFML parser testunit group.

--- tests/unittests/unittests.cpp
+++ tests/unittests/unittests.cpp
@@ -2,6 +2,7 @@
 #include <textparser.h>
 
 #include <json_definition.json.h>
+#include <cfml_definition.json.h>
 
 
 static int parse(const language_definition &definition, const char *code)
@@ -16,6 +17,11 @@ static int parse_json(const char *code)
     return parse(json_definition, code);
 }
 
+static int parse_cfml(const char *code)
+{
+    return parse(cfml_definition, code);
+}
+
 TEST(parse_JSON, basic) {
     EXPECT_EQ(parse_json(R"()"), 0);
     EXPECT_NE(parse_json(R"([)"), 0);
@@ -24,4 +30,8 @@ TEST(parse_JSON, basic) {
     EXPECT_EQ(parse_json(R"(})"), 0);
     EXPECT_EQ(parse_json(R"({})"), 0);
     EXPECT_EQ(parse_json(R"([1])"), 0);
+}
+
+TEST(parse_CFML, basic) {
+    EXPECT_EQ(parse_cfml(R"(<cfset a = "#a#" />)"), 0);
 }
