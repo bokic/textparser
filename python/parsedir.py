6896bb3fa6479cfb8fd0f3699a748ce6b8adc8a1
Boris Barbulovski <bbarbulovski@gmail.com>
2026-01-12 23:51:48 +0100

    Add parsedir.py

---
+++ python/parsedir.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from textparser import TextParser
+import time
+import sys
+import os
+
+def recursiveParseDirectory(definition_file, dir):
+    parser = TextParser(definition_file)
+    for file in os.listdir(dir):
+        if file.endswith(".cfm") or file.endswith(".cfc"):
+            with open(dir + "/" + file, "r") as f:
+                text = f.read()
+            print("Parsing " + dir + "/" + file + "...", end="")
+            start_ns = time.perf_counter_ns()
+            tree = parser.parse(text)
+            end_ns = time.perf_counter_ns()
+            elapsed_ns = end_ns - start_ns
+            print(f" done in {elapsed_ns / 1e6:.3f} ms")
+        elif os.path.isdir(dir + "/" + file):
+            recursiveParseDirectory(definition_file, dir + "/" + file)
+
+def main(args):
+
+    if len(args) < 2:
+        print("Usage: parse.py <definition_file> <directory>")
+        sys.exit(1)
+
+    definition_file = args[0]
+    directory = args[1]
+
+    recursiveParseDirectory(definition_file, directory)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
