e6eaa0e02da0c39d49d6485089b5a12e8d793436
Boris Barbulovski <bbarbulovski@gmail.com>
2026-01-31 20:14:23 +0100

    Update qscintilla test project.

--- tests/qscintilla/mainwindow.h
+++ tests/qscintilla/mainwindow.h
@@ -17,8 +17,17 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_textEdit_textChanged();
+    void on_textEdit_marginClicked(int margin, int line, const Qt::KeyboardModifiers &state);
+
+    void on_textEdit_SCN_ZOOM();
+
+    void on_textEdit_linesChanged();
+
+    void updatePanelWidth();
 
 private:
     Ui::MainWindow *ui;
+    int m_breakpointMarker = 0;
+    int m_breakpointPendingMarker = 0;
+    int m_breakpointDisabledMarker = 0;
 };
