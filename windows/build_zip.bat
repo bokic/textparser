1104528527b909a7a20c798e06dd6698b5b4b528
Boris Barbulovski <bbarbulovski@gmail.com>
2026-06-05 05:02:17 +0200

    Remove cfml_validator

--- windows/build_zip.bat
+++ windows/build_zip.bat
@@ -7,7 +7,6 @@ for /f "delims=" %%i in ('git describe --tags --dirty') do set git_describe=%%i
 mkdir textparser\include
 
 copy ..\bin\ccat.exe textparser
-copy ..\bin\cfml_validator.exe textparser
 copy ..\bin\textparser.exe textparser
 
 copy ..\bin\textparser.dll textparser
@@ -25,6 +24,6 @@ copy ..\bin\pcre2-16.dll textparser
 copy ..\bin\pcre2-32.dll textparser
 copy ..\bin\pcre2-posix.dll textparser
 
-tar -a -c -f "textparser-%git_describe%.zip" "textparser\ccat.exe" "textparser\cfml_validator.exe" "textparser\textparser.exe" "textparser\textparser.dll" "textparser\textparser-json.dll"  "textparser\textparser.lib" "textparser\textparser-json.lib" "textparser\include\textparser.h" "textparser\include\textparser-json.h" "textparser\json-c.dll" "textparser\pcre2-8.dll" "textparser\pcre2-16.dll" "textparser\pcre2-32.dll" "textparser\pcre2-posix.dll"
+tar -a -c -f "textparser-%git_describe%.zip" "textparser\ccat.exe" "textparser\textparser.exe" "textparser\textparser.dll" "textparser\textparser-json.dll"  "textparser\textparser.lib" "textparser\textparser-json.lib" "textparser\include\textparser.h" "textparser\include\textparser-json.h" "textparser\json-c.dll" "textparser\pcre2-8.dll" "textparser\pcre2-16.dll" "textparser\pcre2-32.dll" "textparser\pcre2-posix.dll"
 
 rmdir /s /q textparser
