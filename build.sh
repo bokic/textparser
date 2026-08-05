75cbe0494385223f90b535f5a3f4328bb6715609
Boris Barbulovski <bbarbulovski@gmail.com>
2026-08-05 04:46:15 +0200

    Update build.sh script.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ GENERATOR="${GENERATOR:-Ninja}"
 command -v cmake >/dev/null 2>&1 || { echo >&2 "cmake is required but not installed. Aborting."; exit 1; }
 command -v python3 >/dev/null 2>&1 || { echo >&2 "python3 is required but not installed. Aborting."; exit 1; }
 
-definitions/regenerate.sh
+(cd  definitions; ./regenerate.sh)
 
 if [ "$GENERATOR" = "Ninja" ]; then
     command -v ninja >/dev/null 2>&1 || { echo >&2 "ninja is required for Ninja generator but not installed. Falling back to Unix Makefiles."; GENERATOR="Unix Makefiles"; }
